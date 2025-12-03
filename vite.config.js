ce828b6c0c5d6f382054fd1968d8aadc6e6874f9
Eduardo Agostini <eduardo@trippple.co.nz>
2025-12-03 17:08:20 +1300

    General
    Tweaking icons and providing a 404 fallback for **GitHub Pages**.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
+import fs from 'fs';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [tailwindcss(), vue()],
+    plugins: [tailwindcss(), vue(), {
+        apply: 'build',
+        name: '404',
+
+        closeBundle() {
+            const dist = path.resolve(__dirname, 'dist');
+
+            fs.copyFileSync(
+                path.join(dist, 'index.html'),
+                path.join(dist, '404.html'),
+            );
+        }
+    }],
 });
