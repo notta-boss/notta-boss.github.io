7ad4044bd30da779e69e913ebf020e9bedd094c8
Eduardo Agostini <eduardo@trippple.co.nz>
2025-12-04 23:17:49 +1300

    Google Analytics
    Implementing it with a proper vendor package.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 import './style.css';
 
 import { createApp } from 'vue';
+import { createGtag } from 'vue-gtag';
 import { router } from './services';
 import App from './App.vue';
+
+const gtag = createGtag({
+    tagId: 'G-P14ELE0YJZ'
+});
 
 document
     .body
     .insertAdjacentHTML('beforeend', `<div class="min-h-dvh" id="app" />`);
 
 createApp(App)
+    .use(gtag)
     .use(router)
     .mount('#app');
